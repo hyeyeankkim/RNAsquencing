7d44aa49a69ac7c7412f9ecabcf0ad8181893c34
Hye-Yeon Kim <khyeyean0829@gmail.com>
2018-01-03 18:33:21 +0900

    Update find_filename.py
    filename _1 only check

--- find_filename.py
+++ find_filename.py
@@ -11,6 +11,15 @@ def search(dirname):
                 ext = os.path.splitext(full_filename)[-1]
                 if ext == '.gz': 
                     print(full_filename)
+                text = os.path.splitext(full_filename)[-2]		
+                #print(text)
+                text2 = os.path.splitext(text)[-2]
+                #print(text2)
+                text3 = os.path.basename(text2)
+                #print(text3)	
+                if "_1" in text3:
+                    text4=text3
+                    print(text4)
 
     except PermissionError:
         pass
